4712610520e9ef19a8ff753ce2758c9a82875de1
Devin-Bushey <devin.bushey.pdc@gmail.com>
2023-01-29 21:23:46 -0800

    Added Spotify API integration to get a spotify url based on OpenAI response

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const app = express();
 const cors = require("cors");
 require('dotenv').config('./.env');
 const port = 5001;
+const axios = require('axios');
 
 const configuration = new Configuration({
   organization: "org-0vIMGNLnWtexatzeEFq02gIp",
@@ -17,17 +18,68 @@ const openai = new OpenAIApi(configuration);
 app.use(bodyParser.json());
 app.use(cors());
 
+const getSpotifyAuth = async () => {
+
+  const client_id = process.env.SP_CLIENT_ID;
+  const client_secret = process.env.SP_CLIENT_SECRET;
+  const encoded = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+
+  try {
+    const response = await axios({
+      url: 'https://accounts.spotify.com/api/token',
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + encoded
+      },
+      params: {
+        "grant_type": 'client_credentials'
+      }
+
+    });
+    return response.data.access_token;
+  } catch (error) {
+    console.error("Error: POST getAccessToken");
+    console.error(error);
+  }
+
+}
+
+const getSpotifyTrackLink = async (access_token, song, artist) => {
+  try {
+    const response = await axios.get(`https://api.spotify.com/v1/search?q=track:${song}%20artist:${artist}&type=track`, {
+      headers: {
+        'Authorization': `Bearer ${access_token}`
+      }
+    });
+    console.log("song: ", song, "artist: ", artist, "response:", response.data)
+    if (response.data.tracks.items.length > 0) {
+      return response.data.tracks.items[0].external_urls.spotify;
+    } else {
+      return 'No match found';
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 app.post('/', async (req, res) => {
+
   const { message } = req.body;
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `${message}`,
-    max_tokens: 100,
+    prompt: `Give me a song that is accessible through the Canadian Spotify API that I would listen to that is: ${message}`,
+    max_tokens: 25,
     temperature: 0,
   });
-  //console.log(response.data);
   if (response.data.choices[0].text) {
-    res.json({ message: response.data.choices[0].text });
+    const auth = await getSpotifyAuth();
+    const song = response.data.choices[0].text.split(' by ')[0].replace(/"/g, '');
+    const band = response.data.choices[0].text.split(' by ')[1];
+    const link = await getSpotifyTrackLink(auth, song, band);
+    res.json({
+      message: response.data.choices[0].text,
+      link: link
+    });
   }
 });
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 const App = () => {
   const [message, setMessage] = useState('');
-  const [response, setResponse] = useState('');
+  const [response, setResponse] = useState({message: '', link: ''});
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -15,11 +15,13 @@ const App = () => {
       body: JSON.stringify({ message }),
     })
     .then((res) => res.json())
-    .then((data) => setResponse(data.message));
+    .then((data) => setResponse({message: data.message, link: data.link}));
+    
   };
 
   return (
     <div className="App">
+      <p>What do you want to listen to?</p>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -28,7 +30,8 @@ const App = () => {
         />
         <button type="submit">Send</button>
       </form>
-      <p>{response}</p>
+      {response.message && <p>{response.message}</p>}
+      {response.link != '' && response.link != "No match found" ? <a href={response.link} target="_blank">Listen on Spotify!</a> : response.message && <p>Sorry, couldn't find a match on Spotify :(</p>}
     </div>
   );
 }
